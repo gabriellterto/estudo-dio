05be443d22f5b527f69ec96fa4e22f50a65413c4
Gabriel Terto <gabrielterto1002@gmail.com>
2024-10-22 10:49:38 -0300

    Todos os metodos como public

---
+++ bootcamp-java-claro/programacao-orientada-objetos/fundamentos/visibilidade-dos-recursos/src/lanchonete/atendimento/cozinha/Cozinheiro.java
@@ -0,0 +1,55 @@
+package lanchonete.atendimento.cozinha;
+
+import lanchonete.atendimento.Atendente;
+
+public class Cozinheiro {
+    //pode ser default
+    public void adicionarLancheNoBalcao() {
+        System.out.println("ADICIONANDO LANCHE NATURAL HAMBURGUER NO BALCAO");
+    }
+    //pode ser default
+    public void adicionarSucoNoBalcao() {
+        System.out.println("ADICIONANDO SUCO NO BALCAO");
+    }
+    //pode ser default
+    public void adicionarComboNoBalcao() {
+        adicionarLancheNoBalcao();
+        adicionarSucoNoBalcao();
+    }
+    //pode ser default
+    public void prepararLanche() {
+        System.out.println("PREPARANDO LANCHE TIPO HAMBURGUER");
+    }
+    //pode ser default
+    public void prepararVitamina() {
+        System.out.println("PREPARANDO SUCO");
+    }
+    public void prepararCombo() {
+        prepararLanche();
+        prepararVitamina();
+    }
+    public void selecionarIngredientesLanche() {
+        System.out.println("SELECIONANDO O PÃO, SALADA, OVO E CARNE");
+    }
+    public void selecionarIngredientesVitamina() {
+        System.out.println("SELECIONANDO FRUTA, LEITE E SUCO");
+    }
+    public void lavarIngredientes() {
+        System.out.println("LAVANDO INGREDIENTES");
+    }
+    public void baterVitaminaLiquidificador() {
+        System.out.println("BATENDO VITAMINA LIQUIDIFICADOR");
+    }
+    public void fritarIngredientesLanche() {
+        System.out.println("FRITANDO A CARNE E OVO PARA O HAMBURGUER");
+    }
+    public void pedirParaTrocarGas(Atendente meuAmigo) {
+        meuAmigo.trocarGas();
+    }
+    public void pedirParaTrocarGas(Almoxarife meuAmigo) {
+        meuAmigo.trocarGas();
+    }
+    public void pedirIngredientes(Almoxarife almoxarife) {
+        almoxarife.entregarIngredientes();
+    }
+}
