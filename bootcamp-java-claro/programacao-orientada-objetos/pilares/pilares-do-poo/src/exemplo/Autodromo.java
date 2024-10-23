253f13b3917e7e250b8290e4d476228412210335
Gabriel Terto <gabrielterto1002@gmail.com>
2024-10-23 10:04:41 -0300

    Encapsulamento

---
+++ bootcamp-java-claro/programacao-orientada-objetos/pilares/pilares-do-poo/src/exemplo/Autodromo.java
@@ -0,0 +1,8 @@
+package exemplo;
+
+public class Autodromo {
+    public static void main(String[] args) {
+        Carro jeep = new Carro();
+        jeep.ligar();
+    }
+}
---
+++ bootcamp-java-claro/programacao-orientada-objetos/pilares/pilares-do-poo/src/exemplo/Carro.java
@@ -0,0 +1,15 @@
+package exemplo;
+
+public class Carro {
+    public void ligar() {
+        confereCombustivel();
+        confereCambio();
+        System.out.println("CARRO LIGADO");
+    }
+    private void confereCombustivel() {
+        System.out.println("CONFERINDO COMBUSTIVEL");
+    }
+    private void confereCambio() {
+        System.out.println("CONFERINDO CAMBIO EM P");
+    }
+}
