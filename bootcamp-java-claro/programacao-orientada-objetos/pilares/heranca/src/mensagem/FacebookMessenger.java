a04ca4a7518187b4211cee47a0fa9d3d1541f3e5
Gabriel Terto <gabrielterto1002@gmail.com>
2024-10-23 10:51:14 -0300

    Usando o extends para as classes herdarem os metodos da classe ServicoMensagemInstantanea

--- bootcamp-java-claro/programacao-orientada-objetos/pilares/heranca/src/mensagem/FacebookMessenger.java
+++ bootcamp-java-claro/programacao-orientada-objetos/pilares/heranca/src/mensagem/FacebookMessenger.java
@@ -1,18 +1,5 @@
 package mensagem;
 
-public class FacebookMessenger {
-    public void enviarMensagem() {
-        validarConectadoInternet();
-        System.out.println("Enviando mensagem pelo Facebook Messenger");
-        salvarHistoricoMensagem();
-    }
-    public void receberMensagem() {
-        System.out.println("Recebendo mensagem pelo Facebook Messenger");
-    }
-    private void validarConectadoInternet() {
-        System.out.println("Validando se está conectado a internet");
-    }
-    private void salvarHistoricoMensagem() {
-        System.out.println("Salvando o histórico da mensagem");
-    }
+public class FacebookMessenger extends ServicoMensagemInstantanea{
+
 }
--- bootcamp-java-claro/programacao-orientada-objetos/pilares/heranca/src/mensagem/Telegram.java
+++ bootcamp-java-claro/programacao-orientada-objetos/pilares/heranca/src/mensagem/Telegram.java
@@ -1,18 +1,5 @@
 package mensagem;
 
-public class Telegram {
-    public void enviarMensagem() {
-        validarConectadoInternet();
-        System.out.println("Enviando mensagem pelo Telegram");
-        salvarHistoricoMensagem();
-    }
-    public void receberMensagem() {
-        System.out.println("Recebendo mensagem pelo Telegram");
-    }
-    private void validarConectadoInternet() {
-        System.out.println("Validando se está conectado a internet");
-    }
-    private void salvarHistoricoMensagem() {
-        System.out.println("Salvando o histórico da mensagem");
-    }
+public class Telegram extends ServicoMensagemInstantanea{
+
 }
