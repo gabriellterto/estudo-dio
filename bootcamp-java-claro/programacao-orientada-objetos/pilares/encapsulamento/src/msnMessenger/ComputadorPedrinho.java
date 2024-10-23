ea350f330d62abee930970e0e575f29ee33ffc29
Gabriel Terto <gabrielterto1002@gmail.com>
2024-10-23 10:35:19 -0300

    Encapsulando os metodos validarConectadoInternet e salvarHistoricoMensagem da classe MsnMessenger

--- bootcamp-java-claro/programacao-orientada-objetos/pilares/encapsulamento/src/msnMessenger/ComputadorPedrinho.java
+++ bootcamp-java-claro/programacao-orientada-objetos/pilares/encapsulamento/src/msnMessenger/ComputadorPedrinho.java
@@ -4,9 +4,7 @@ public class ComputadorPedrinho {
     public static void main(String[] args) {
         MsnMessenger msn = new MsnMessenger();
 
-        msn.validarConectadoInternet();
         msn.enviarMensagem();
-        msn.salvarHistoricoMensagem();
 
         msn.receberMensagem();
     }
