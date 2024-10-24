31b36e5dc3544cc6ebc30f9fb151447a98564ea4
Gabriel Terto <gabrielterto1002@gmail.com>
2024-10-24 10:53:34 -0300

    Adicionando a classe Iphone que implementa as interfaces

---
+++ bootcamp-java-claro/programacao-orientada-objetos/desafio-de-projeto/modelando-iphone/src/model/Iphone.java
@@ -0,0 +1,52 @@
+package model;
+
+import interfaces.AparelhoTelefonico;
+import interfaces.NavegadorInternet;
+import interfaces.ReprodutorMusical;
+
+public class Iphone implements ReprodutorMusical, AparelhoTelefonico, NavegadorInternet {
+    @Override
+    public void ligar(String numero) {
+        System.out.println("Ligando para: " + numero);
+    }
+
+    @Override
+    public void atender() {
+        System.out.println("Atendendo a chamada...");
+    }
+
+    @Override
+    public void iniciarCorreioVoz() {
+        System.out.println("Iniciando o correio de voz...");
+    }
+
+    @Override
+    public void exibirPagina(String url) {
+        System.out.println("Exibindo a pagina: " + url);
+    }
+
+    @Override
+    public void adicionarNovaAba() {
+        System.out.println("Nova aba adicionada");
+    }
+
+    @Override
+    public void atualizarPagina() {
+        System.out.println("Pagina atualizada");
+    }
+
+    @Override
+    public void tocar() {
+        System.out.println("Tocando música");
+    }
+
+    @Override
+    public void pausar() {
+        System.out.println("Pausando música");
+    }
+
+    @Override
+    public void selecionarMusica() {
+        System.out.println("Selecionando música");
+    }
+}
