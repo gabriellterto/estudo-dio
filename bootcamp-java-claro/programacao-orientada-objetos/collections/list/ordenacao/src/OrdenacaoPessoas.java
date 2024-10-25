01e4f8f83647606aa9cacba67656904964056cc9
Gabriel Terto <gabrielterto1002@gmail.com>
2024-10-25 15:39:20 -0300

    Adicionando as classes Pessoa e OrdenacaoPessoas

---
+++ bootcamp-java-claro/programacao-orientada-objetos/collections/list/ordenacao/src/OrdenacaoPessoas.java
@@ -0,0 +1,29 @@
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class OrdenacaoPessoas {
+    //atributo
+    private List<Pessoa> pessoaList;
+
+    public OrdenacaoPessoas() {
+        this.pessoaList = new ArrayList<>();
+    }
+
+    public void adicionarPessoa(String nome, int idade, double altura) {
+        pessoaList.add(new Pessoa(nome, idade, altura));
+    }
+
+    public List<Pessoa> ordenarPorIdade() {
+        List<Pessoa> pessoaPorIdade = new ArrayList<>(pessoaList);
+        Collections.sort(pessoaPorIdade);
+        return pessoaPorIdade;
+    }
+
+    public List<Pessoa> ordenarPorAltura() {
+        List<Pessoa> pessoasPorAltura = new ArrayList<>(pessoaList);
+        Collections.sort(pessoasPorAltura, new ComparatorPorAltura());
+        return pessoasPorAltura;
+    }
+}
+
