0e7241784d33bcc0d77ea4b6602922f3f574d8e6
Gabriel Terto <gabrielterto1002@gmail.com>
2024-10-25 15:19:02 -0300

    Testando os métodos do exercicio 2 - Soma de numeros

--- bootcamp-java-claro/programacao-orientada-objetos/collections/list/pesquisa/src/SomaNumeros.java
+++ bootcamp-java-claro/programacao-orientada-objetos/collections/list/pesquisa/src/SomaNumeros.java
@@ -63,4 +63,18 @@ public class SomaNumeros {
             System.out.println("A lista está vazia");
         }
     }
+
+    public static void main(String[] args) {
+        SomaNumeros somaNumeros = new SomaNumeros();
+        somaNumeros.adicionarNumero(5);
+        somaNumeros.adicionarNumero(0);
+        somaNumeros.adicionarNumero(0);
+        somaNumeros.adicionarNumero(-2);
+        somaNumeros.adicionarNumero(10);
+        System.out.println("Números adicionados:");
+        somaNumeros.exibirNumeros();
+        System.out.println("Soma dos números = " + somaNumeros.calcularSoma());
+        System.out.println("Maior número = " + somaNumeros.encontrarMaiorNumero());
+        System.out.println("Menor número = " + somaNumeros.encontrarMenorNumero());
+    }
 }
