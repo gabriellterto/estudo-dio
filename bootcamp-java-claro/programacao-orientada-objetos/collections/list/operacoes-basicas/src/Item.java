7b03c736669b6b21313dbdc0bf812166bf7d8834
Gabriel Terto <gabrielterto1002@gmail.com>
2024-10-25 09:55:28 -0300

    Exercicio CarrinhoDeCompras

---
+++ bootcamp-java-claro/programacao-orientada-objetos/collections/list/operacoes-basicas/src/Item.java
@@ -0,0 +1,32 @@
+public class Item {
+    private String nome;
+    private double preco;
+    private int quantidade;
+
+    public Item(String nome, double preco, int quantidade) {
+        this.nome = nome;
+        this.preco = preco;
+        this.quantidade = quantidade;
+    }
+
+    public double getPreco() {
+        return preco;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public int getQuantidade() {
+        return quantidade;
+    }
+
+    @Override
+    public String toString() {
+        return "Item{" +
+                "nome='" + this.nome + '\'' +
+                ", preco=" + this.preco +
+                ", quantidade=" + this.quantidade +
+                '}';
+    }
+}
