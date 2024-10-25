fbfe74cd15a461cb651bac7f192693877e2499ca
Gabriel Terto <gabrielterto1002@gmail.com>
2024-10-25 15:17:34 -0300

    Fazendo o exercicio 2 - SOMA DE NÚMEROS

--- bootcamp-java-claro/programacao-orientada-objetos/collections/list/pesquisa/src/SomaNumeros.java
+++ bootcamp-java-claro/programacao-orientada-objetos/collections/list/pesquisa/src/SomaNumeros.java
@@ -1,3 +1,66 @@
-public class SomaNumeros {
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
 
+public class SomaNumeros {
+    private List<Integer> numeros = new ArrayList();
+
+    public void adicionarNumero(int numero) {
+        this.numeros.add(numero);
+    }
+
+    public int calcularSoma() {
+        int soma = 0;
+        if(!this.numeros.isEmpty()) {
+            Integer numero = 0;
+            for(Iterator i = this.numeros.iterator(); i.hasNext(); soma += numero) {
+                numero = (Integer)i.next();
+            }
+        }
+        return soma;
+    }
+
+    public int encontrarMaiorNumero() {
+        int maiorNumero = Integer.MIN_VALUE;
+        if(!this.numeros.isEmpty()) {
+            Iterator i = this.numeros.iterator();
+
+            while(i.hasNext()) {
+                Integer numero = (Integer)i.next();
+                if(numero >= maiorNumero) {
+                    maiorNumero = numero;
+                }
+            }
+
+            return maiorNumero;
+        } else {
+            throw  new RuntimeException("A lista está vazia!");
+        }
+    }
+
+    public int encontrarMenorNumero() {
+        int menorNumero = Integer.MAX_VALUE;
+        if(!this.numeros.isEmpty()) {
+            Iterator i = this.numeros.iterator();
+
+            while(i.hasNext()) {
+                Integer numero = (Integer)i.next();
+                if(numero <= menorNumero) {
+                    menorNumero = numero;
+                }
+            }
+
+            return menorNumero;
+        } else {
+            throw new RuntimeException("A lista está vazia");
+        }
+    }
+
+    public void exibirNumeros() {
+        if(!this.numeros.isEmpty()) {
+            System.out.println(this.numeros);
+        } else {
+            System.out.println("A lista está vazia");
+        }
+    }
 }
