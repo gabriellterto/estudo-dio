901c18e7aff0c94c1e0423cee3ea04c79d7d2a4f
Gabriel Terto <gabrielterto1002@gmail.com>
2024-10-28 13:54:34 -0300

    Gerando um equals and hashcode

--- bootcamp-java-claro/programacao-orientada-objetos/collections/set/operacoes-basicas/src/Convidado.java
+++ bootcamp-java-claro/programacao-orientada-objetos/collections/set/operacoes-basicas/src/Convidado.java
@@ -1,3 +1,5 @@
+import java.util.Objects;
+
 public class Convidado {
     //atributos
     private String nome;
@@ -14,6 +16,19 @@ public class Convidado {
 
     public int getCodigoConvite() {
         return codigoConvite;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Convidado convidado = (Convidado) o;
+        return getCodigoConvite() == convidado.getCodigoConvite();
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(getCodigoConvite());
     }
 
     @Override
