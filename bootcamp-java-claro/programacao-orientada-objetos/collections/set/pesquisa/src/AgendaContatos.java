4f2e162f888ef9acccd31fb916d911b80f6f27df
Gabriel Terto <gabrielterto1002@gmail.com>
2024-10-28 14:47:10 -0300

    Adicionando as classes Contato e AgendaContatos

---
+++ bootcamp-java-claro/programacao-orientada-objetos/collections/set/pesquisa/src/AgendaContatos.java
@@ -0,0 +1,54 @@
+import java.util.HashSet;
+import java.util.Set;
+
+public class AgendaContatos {
+    //atributo
+    private Set<Contato> contatoSet;
+
+    public AgendaContatos() {
+        this.contatoSet = new HashSet<>();
+    }
+
+    public void adicionarContato(String nome, int numero) {
+        contatoSet.add(new Contato(nome, numero));
+    }
+
+    public void exibirContatos() {
+        if(!contatoSet.isEmpty()) {
+            System.out.println(contatoSet);
+        } else {
+            System.out.println("O conjunto está vazio!");
+        }
+    }
+
+    public Set<Contato> pesquisarPorNome(String nome) {
+        Set<Contato> contatosPorNome = new HashSet<>();
+        if(!contatoSet.isEmpty()) {
+            for(Contato c : contatoSet) {
+                if(c.getNome().startsWith(nome)) {
+                    contatosPorNome.add(c);
+                }
+            }
+            return contatosPorNome;
+        } else {
+            throw new RuntimeException("O conjunto está vazio!");
+        }
+    }
+
+    public Contato atualizarNumeroContato(String nome, int novoNumero) {
+        Contato contatoAtualizado = null;
+        if(!contatoSet.isEmpty()) {
+            for(Contato c : contatoSet) {
+                if(c.getNome().equalsIgnoreCase(nome)) {
+                    c.setNumero(novoNumero);
+                    contatoAtualizado = c;
+                    break;
+                }
+            }
+            return contatoAtualizado;
+        } else {
+            throw new RuntimeException("O conjunto está vazio!");
+        }
+    }
+
+}
