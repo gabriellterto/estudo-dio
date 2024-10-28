22b7de931e75be5fd4524383fa2a4fc49c81ddad
Gabriel Terto <gabrielterto1002@gmail.com>
2024-10-28 20:50:05 -0300

    Corrigindo nome dos metodos exibirProdutosPorPreco e exibirProdutosPorNome e testando os metodos

--- bootcamp-java-claro/programacao-orientada-objetos/collections/set/ordenacao/src/CadastroProdutos.java
+++ bootcamp-java-claro/programacao-orientada-objetos/collections/set/ordenacao/src/CadastroProdutos.java
@@ -13,7 +13,7 @@ public class CadastroProdutos {
         produtoSet.add(new Produto(cod, nome, preco, quantidade));
     }
 
-    public Set<Produto> exibirPordutosPorNome() {
+    public Set<Produto> exibirProdutosPorNome() {
         Set<Produto> produtosPorNome = new TreeSet<>(produtoSet);
         if(!produtoSet.isEmpty()) {
             return produtosPorNome;
@@ -22,7 +22,7 @@ public class CadastroProdutos {
         }
     }
 
-    public Set<Produto> exibirProdutoPorPreco() {
+    public Set<Produto> exibirProdutosPorPreco() {
         Set<Produto> produtosPorPreco = new TreeSet<>(new ComparatorPorPreco());
         if(!produtoSet.isEmpty()) {
             produtosPorPreco.addAll(produtoSet);
@@ -32,4 +32,23 @@ public class CadastroProdutos {
         }
     }
 
+    public static void main(String[] args) {
+        // Criando uma instância do CadastroProdutos
+        CadastroProdutos cadastroProdutos = new CadastroProdutos();
+
+        // Adicionando produtos ao cadastro
+        cadastroProdutos.adicionarProduto(1L, "Smartphone", 1000d, 10);
+        cadastroProdutos.adicionarProduto(2L, "Notebook", 1500d, 5);
+        cadastroProdutos.adicionarProduto(1L, "Mouse", 30d, 20);
+        cadastroProdutos.adicionarProduto(4L, "Teclado", 50d, 15);
+
+        // Exibindo todos os produtos no cadastro
+        System.out.println(cadastroProdutos.produtoSet);
+
+        // Exibindo produtos ordenados por nome
+        System.out.println(cadastroProdutos.exibirProdutosPorNome());
+
+        // Exibindo produtos ordenados por preço
+        System.out.println(cadastroProdutos.exibirProdutosPorPreco());
+    }
 }
