39c11666e2814c959d149031836e2b65c76a30ca
Gabriel Terto <gabrielterto1002@gmail.com>
2024-10-28 20:48:27 -0300

    Adicionando as classes Produto CadastroProdutos

---
+++ bootcamp-java-claro/programacao-orientada-objetos/collections/set/ordenacao/src/CadastroProdutos.java
@@ -0,0 +1,35 @@
+import java.util.HashSet;
+import java.util.Set;
+import java.util.TreeSet;
+
+public class CadastroProdutos {
+    private Set<Produto> produtoSet;
+
+    public CadastroProdutos() {
+        this.produtoSet = new HashSet<>();
+    }
+
+    public void adicionarProduto(long cod, String nome, double preco, int quantidade) {
+        produtoSet.add(new Produto(cod, nome, preco, quantidade));
+    }
+
+    public Set<Produto> exibirPordutosPorNome() {
+        Set<Produto> produtosPorNome = new TreeSet<>(produtoSet);
+        if(!produtoSet.isEmpty()) {
+            return produtosPorNome;
+        } else {
+            throw new RuntimeException("O conjunto está vazio!");
+        }
+    }
+
+    public Set<Produto> exibirProdutoPorPreco() {
+        Set<Produto> produtosPorPreco = new TreeSet<>(new ComparatorPorPreco());
+        if(!produtoSet.isEmpty()) {
+            produtosPorPreco.addAll(produtoSet);
+            return produtosPorPreco;
+        } else {
+            throw new RuntimeException("O conjunto está vazio!");
+        }
+    }
+
+}
