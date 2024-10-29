f9c1b7bdd13b5832aefaa1176f663319ce61a0b1
Gabriel Terto <gabrielterto1002@gmail.com>
2024-10-29 09:55:08 -0300

    Resolvendo exercicio 2

--- bootcamp-java-claro/programacao-orientada-objetos/collections/map/pesquisa/src/ContagemPalavras.java
+++ bootcamp-java-claro/programacao-orientada-objetos/collections/map/pesquisa/src/ContagemPalavras.java
@@ -1,2 +1,42 @@
+import java.util.HashMap;
+import java.util.Map;
+
 public class ContagemPalavras {
+    private Map<String, Integer> palavras;
+
+    public ContagemPalavras() {
+        this.palavras = new HashMap<>();
+    }
+
+    public void adicionarPalavra(String palavra, Integer contagem) {
+        palavras.put(palavra, contagem);
+    }
+
+    public void removerPalavra(String palavra) {
+        if(!palavras.isEmpty()) {
+            palavras.remove(palavra);
+        } else {
+            System.out.println("Palavra não encontrada.");
+        }
+    }
+
+    public int exibirContagemPalavras() {
+        int contagemTotal = 0;
+        for(int contagem : palavras.values()) {
+            contagemTotal += contagem;
+        }
+        return contagemTotal;
+    }
+
+    public String encontrarPalavraMaisFrequente() {
+        String palavraMaisFrequente = null;
+        int maiorContagem = 0;
+        for(Map.Entry<String, Integer> entry : palavras.entrySet()) {
+            if(entry.getValue() > maiorContagem) {
+                maiorContagem = entry.getValue();
+                palavraMaisFrequente = entry.getKey();
+            }
+        }
+        return palavraMaisFrequente;
+    }
 }
