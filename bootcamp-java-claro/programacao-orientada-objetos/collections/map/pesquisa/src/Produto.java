d22e10c3f3868a0be74d69cac7ca1b6ec3d38d79
Gabriel Terto <gabrielterto1002@gmail.com>
2024-10-29 09:43:13 -0300

    Estoque de Produtos com Preço

--- bootcamp-java-claro/programacao-orientada-objetos/collections/map/pesquisa/src/Produto.java
+++ bootcamp-java-claro/programacao-orientada-objetos/collections/map/pesquisa/src/Produto.java
@@ -1,2 +1,32 @@
 public class Produto {
+    private String nome;
+    private double preco;
+    private int quantidade;
+
+    public Produto(String nome, double preco, int quantidade) {
+        this.nome = nome;
+        this.preco = preco;
+        this.quantidade = quantidade;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public int getQuantidade() {
+        return quantidade;
+    }
+
+    public double getPreco() {
+        return preco;
+    }
+
+    @Override
+    public String toString() {
+        return "Produto{" +
+                "nome='" + nome + '\'' +
+                ", preco=" + preco +
+                ", quantidade=" + quantidade +
+                '}';
+    }
 }
